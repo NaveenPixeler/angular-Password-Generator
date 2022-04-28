e7c6f645d097faec7cc5b1666b06f48707cee3fa
NaveenPixeler <cgnaveen022@gmail.com>
2022-04-28 18:04:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pyi6qg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pyi6qg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const Symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += Symbols;
+    }
+
+    let generatePassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+    this.password = generatePassword;
+  }
+  // console.log(`
+  //   Includes Letters: ${this.includeLetters}
+  //   Includes Numbers: ${this.includeNumbers}
+  //   Includes Symbols: ${this.includeSymbols}
+
+  // `);
+  // this.password = 'My Password!!!'
+
+  // getPassword(){
+  //   return this.password;
+  // }
+}
